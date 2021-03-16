6444566b2f1648825f5d5c5020fc8f426fa5ddfe
kimchanhyung98 <kimchanhyung98@gmail.com>
2021-03-16 23:11:23 +0900

    Update HomeController.php
    #2

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -19,11 +19,24 @@ class HomeController extends Controller
      */
     public function store(Request $request, Faker $faker): JsonResponse
     {
-        $url = Url::create([
-            'name' => $faker->unique()->regexify('[a-z0-9]{6}'),
-            'origin' => $request->url,
-            'ip' => $request->ip(),
-        ]);
+        // 기존에 등록된 URL인지 확인
+        $url = Url::where('origin', $request->url)->first();
+
+        if(!isset($url)) {
+            // 중복되지 않는 난수 생성
+            do {
+                $name = $faker->regexify('[a-z0-9]{6}');
+                $data = Url::where('name', $name)->exists();
+            }
+            while ($data);
+
+            // 단축 URL 생성
+            $url = Url::create([
+                'name' => $name,
+                'origin' => $request->url,
+                'ip' => $request->ip(),
+            ]);
+        }
 
         return response()->json(['name' => $url->name]);
     }
@@ -37,8 +50,9 @@ class HomeController extends Controller
     public function show($name): View
     {
         $url = Url::where('name', $name)->first();
+        $url->increment('total');
 
-        /* route에서 직접 redirect 하지 않고 GA 실행 후 이동 */
+        // route에서 직접 redirect 하지 않고 GA 실행 후 이동
         return view('redirect', ['origin' => $url->origin]);
     }
 }
