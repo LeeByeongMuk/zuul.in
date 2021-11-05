b50daedb27350f1cc1ce2287bcd43ad69aa2def8
LeeByeongMuk <mansogod@gmail.com>
2021-11-05 21:01:21 +0900

    Update viewport

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,7 +15,7 @@
         <!-- End Google Tag Manager -->
 
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width">
         <meta name="csrf-token" content="{{ csrf_token() }}" />
 
         <title>{{ config('app.name') }}</title>
